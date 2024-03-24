e6ee569c85f3c2bf56cf8255cacae20fec8ea267
lcy <2012074962@qq.com>
2024-03-24 10:39:16 +0800

    添加aardio工程

---
+++ AutoCarOCR/py/AutoOCR.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 15:48:01 2024
+
+@author: LCY
+"""
+import os
+import subprocess
+
+UIMPath="D:\\Umi-OCR_Paddle_v2.1.0\\Umi-OCR.exe"
+
+imagePath = 'C:\\Users\\LCY\\Desktop\\te\\'
+
+
+# 单个图片OCR至指定文件中(两个参数绝对路径)
+def fileOCR(filepath,fileResult):
+    # cmd1 = UIMPath+ " --path "+filepath+"  -->>"+fileResult
+    
+    cmd1 = UIMPath+ " --path "+filepath+"  -->>"+fileResult
+    #os.system(cmd1)
+    return subprocess.run(cmd1,shell=True ,creationflags=subprocess.CREATE_NO_WINDOW)
+    pass
+
+
+# 打开txt（） 如没有则创建文件并写入（），关闭
+def writerFile(filePath,txt):
+    file=open(filePath,'a')
+    file.write(txt)
+    file.close()
+    return 
+
+
+# 读取目录下所有一级文件夹数量返回列表
+def listdir(dirs):
+    listdirs = []
+    for name in os.listdir(dirs):
+        if os.path.isdir(os.path.join(dirs, name)):
+            listdirs.append(name)
+    return listdirs
+
+#读取目录下所有一级文件数量，返回列表
+def listfile(dirs):
+    listfiles = []
+    for name in os.listdir(dirs):
+        if os.path.isfile(os.path.join(imagePath,dirs,name)):
+            listfiles.append(name)
+    return listfiles
+
+
+
+# 遍历所有一级文件夹内图片OCR并保存至目录同级文件夹txt中（每个文件夹添加开始结束标识符）
+def run():
+    
+    global UIMPath,imagePath
+    UIMPath=str(UIMPath)
+    imagePath=str(imagePath)
+    resultFilePath = os.path.join(imagePath,"OCRResult.txt")
+    
+    for dirs in listdir(imagePath):
+        writerFile(resultFilePath,"Start"+dirs+"\n")
+        for files in listfile(os.path.join(imagePath, dirs)):
+            fileOCR(os.path.join(imagePath, dirs,files),resultFilePath)
+            #print(fileOCR(os.path.join(imagePath, dirs,files),resultFilePath))
+        writerFile(resultFilePath,"End"+dirs+"\n")
+    return 0;
---
+++ AutoDanBao/AutoDanBao.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  9 12:54:33 2024
+
+@author: LCY
+"""
+
+import subprocess
+from DrissionPage import ChromiumPage, ChromiumOptions
+import DanBao
+
+
+# 第一次运行需要
+def firstRun():
+    ChromiumOptions().use_system_user_path().save()
+    co = ChromiumOptions().use_system_user_path()
+
+bowserpath='D:\PSBCBrowser\Program\PSBCBrowser.exe'
+bowserpath1='\"C:\Program Files\Google\Chrome\Application\chrome.exe\"'
+# --remote-debugging-port=9222 --remote-allow-origins=*
+
+# 打开浏览器,并打开网页
+def openbowser(_bowserpath=bowserpath,_url='https://22.230.88.182/login.html'):
+    cmd1 = _bowserpath + ' --remote-debugging-port=9222 --remote-allow-origins=*'
+    subprocess.run(cmd1,shell=True ,creationflags=subprocess.CREATE_NO_WINDOW)
+    co = ChromiumOptions().set_local_port(9222)
+    page = ChromiumPage(co)
+    page.get(_url)
+    return 0
+
+
+#点击自动化用印
+def useY():
+    co = ChromiumOptions().set_local_port(9222)
+    page = ChromiumPage(co)
+    tab=page.get_tab(page.find_tabs(title='印章'))
+    if tab.s_ele('#tabsPageHeaderContent').child('@title:人工授权用印'):
+        tab.ele('#tabsPageHeaderContent').child('@title:人工授权用印').click()
+        pass
+    else:
+        if tab.s_ele('tag=h3@@text()=自动化用印@@aria-selected=false'):
+            tab.ele('tag=h3@@text()=自动化用印@@aria-selected=false').click()
+        if tab.s_ele('tag=span@@id=2Tree_10_switch@@class:center_close'):
+            tab.ele('tag=span@@id=2Tree_10_switch').click()
+        tab.ele('#2Tree_12_span').click()
+    
+    tab.ele('tag=input@@data-index=0').click()
+    tab.ele('tag=button@@text():用印@@data-trancode=505011').click()
+
+    page.wait.ele_displayed(page.ele('tag=div@@class:dialogContent'))
+    #_f=page.ele('tag=div@@class:dialogContent')
+    tab.ele('#btn_submit_dialog').click()
+    # page.wait.load_start()
+    # page.wait(5)
+    page.wait.ele_displayed(page.ele('@text():请确认已放入凭证'))
+    page.ele('@text():请确认已放入凭证').parent().child('@text():已放入').click()
+    
+
+DanBao.dataPledges(DanBao.creatClipboardData(),r'C:\Users\dkzx\Desktop\权证交接\20240321')
+#DanBao.dataPledges(DanBao.creatClipboardData('house'),r'C:\Users\dkzx\Desktop\权证交接\2024\202402\20240218')
+#useY()
+#print('Hello')
