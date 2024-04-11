b8bb55f5e02cf61fbfc4b712dd7b67ff6d43c919
lcy <2012074962@qq.com>
2024-04-12 00:47:09 +0800

    添加pythn OCR代码

--- AutoBdc/py/SortOCR.py
+++ AutoBdc/py/SortOCR.py
@@ -1,6 +1,20 @@
 import os
 import shutil
 import time
+from rapidocr_onnxruntime import RapidOCR
+
+engine = RapidOCR()
+
+img_path=r"E:\文档\郭丽华 刘桂根\IMG_0007.jpg"
+
+result, elapse = engine(img_path, use_det=False, use_cls=False, use_rec=True)
+for i in result:
+    print(i)
+#print(result)
+#print(elapse)
+
+
+
 
 #file_path = ""
 #folder_path = "./_delete_"
